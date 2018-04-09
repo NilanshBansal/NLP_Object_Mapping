047800110ee4d105754c62a9a07a3acae45f9393
Nilansh Bansal <bansalnilansh@gmail.com>
2018-04-09 13:32:21 +0530

    optimization

--- app.js
+++ app.js
@@ -2,43 +2,43 @@ let source = require ('./source.json');
 let part_of_speech_map= (require('./config')).map
 
 const createTargetData = (source) =>{
-    let eachToken={};
+    let token={};
     let targetObj={tokens:[]};
     if(source && source.type == 'RootNode' && source.children && source.children.length>0){
-        source.children.forEach((eachParagraph)=>{
-            if(eachParagraph.type=='ParagraphNode' && eachParagraph.children && eachParagraph.children.length>0){
-                eachParagraph.children.forEach((eachSentence)=>{
-                    if(eachSentence.type=='SentenceNode' && eachSentence.children && eachSentence.children.length>0){
-                        eachSentence.children.forEach((eachWord)=>{
+        source.children.forEach((paragraph)=>{
+            if(paragraph.type=='ParagraphNode' && paragraph.children && paragraph.children.length>0){
+                paragraph.children.forEach((sentence)=>{
+                    if(sentence.type=='SentenceNode' && sentence.children && sentence.children.length>0){
+                        sentence.children.forEach((word)=>{
                             let beginOffset;
                             let contentValue;
-                            if(eachWord.type=='PunctuationNode'){
-                                if(eachWord.position && eachWord.position.start){
-                                    beginOffset=eachWord.position.start.offset
+                            if(word.type=='PunctuationNode'){
+                                if(word.position && word.position.start){
+                                    beginOffset=word.position.start.offset
                                 }
-                                eachToken={
+                                token={
                                     text:{
-                                        content:eachWord.value,
+                                        content:word.value,
                                         beginOffset:beginOffset
                                     },
                                     partOfSpeech:{
                                         tag:'PUNCT'
                                     }
                                 }
-                                targetObj.tokens.push(eachToken);
+                                targetObj.tokens.push(token);
                             }
-                            else if(eachWord.type=='WordNode'){
+                            else if(word.type=='WordNode'){
                                 let tagType;
-                                if(eachWord.position && eachWord.position.start){
-                                    beginOffset=eachWord.position.start.offset
+                                if(word.position && word.position.start){
+                                    beginOffset=word.position.start.offset
                                 }
-                                if(eachWord.children && eachWord.children[0] && eachWord.children[0].type=='TextNode'){
-                                    contentValue=eachWord.children[0].value;
+                                if(word.children && word.children[0] && word.children[0].type=='TextNode'){
+                                    contentValue=word.children[0].value;
                                 }
-                                if(eachWord.data ){
-                                    tagType=part_of_speech_map[eachWord.data.partOfSpeech] || "UNKNOWN"
+                                if(word.data ){
+                                    tagType=part_of_speech_map[word.data.partOfSpeech] || "UNKNOWN"
                                 }  
-                                eachToken={
+                                token={
                                     text:{
                                         content:contentValue,
                                         beginOffset:beginOffset
@@ -48,7 +48,7 @@ const createTargetData = (source) =>{
                                     }
                                     
                                 }
-                                targetObj.tokens.push(eachToken);
+                                targetObj.tokens.push(token);
                                 
                             }
                         })
